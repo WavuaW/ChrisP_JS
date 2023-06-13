56cd69e872873c883b95fac582df6c7d7c03c99a
WavuaW <wavuamuroka@gmail.com>
2023-06-13 17:17:01 +0300

    freecodecamp Js

---
+++ 0x01JS/index.js
@@ -0,0 +1,12 @@
+/*
+Data Types
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+var myName = "Chris" //can be used in the entire program 
+
+myName = 8
+
+let ourName = "freeCodeCamp" //within the scope in which you declare with let
+
+const pi = 3.14 //variable that should never change
