773a01dd972c856a858f2aac5bb44bdcef1bf24c
WavuaW <wavuamuroka@gmail.com>
2023-07-05 06:41:24 +0300

    Local vs global scope

--- 0x01JS/localScope.js
+++ 0x01JS/localScope.js
@@ -6,3 +6,5 @@ function myLocalScope() {
 myLocalScope();
 
 console.log(myVar)
+
+//in the case where a global variable and the local variable have the same names the local variable takes precidence over the global variable and what is outputed falls within the scope of the local variable.
