4f549794ecbabaef3b3cc0580e93f80bffdca39d
WavuaW <wavuamuroka@gmail.com>
2023-07-06 12:34:24 +0300

    equality operator

---
+++ 0x01JS/doubleEqualityOperator.js
@@ -0,0 +1,10 @@
+function testEqual(val) {
+    if (val == 12) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+console.log(testEqual(9));
+
+//correction as this is not an if statement example but a simple equality operator.
