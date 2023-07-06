8c6d6fff220bfc870bdd9da9feb97c83363e0924
WavuaW <wavuamuroka@gmail.com>
2023-07-06 12:32:58 +0300

    strict equal operators

---
+++ 0x01JS/strictEqualityOp.js
@@ -0,0 +1,19 @@
+function testStrict(val) {
+    if (val) {
+        return "Equal";
+    }
+    return "Not Equal"
+}
+
+console.log(testStrict(10))
+
+/*
+the == operator attempts to convert the two options to the same type 
+While the === also checks for the type so for instance:
+3 == 3
+3 == '3'
+will both be evaluated as true.
+While:
+3 === 3 returns true and,
+3 === '3' evaluates as false
+*/
