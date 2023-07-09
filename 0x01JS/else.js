768f6bb65492b64601c0e678b912177553d60568
WavuaW <wavuamuroka@gmail.com>
2023-07-09 19:30:25 +0300

    Else statements

---
+++ 0x01JS/else.js
@@ -0,0 +1,10 @@
+function testElse(val) {
+    var result = '';
+    
+    if (val > 5) {
+        result = "Bigger than 5";
+    } else {
+        result = "5 or smaller";
+    }
+    return result;
+}
