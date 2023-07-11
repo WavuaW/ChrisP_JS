111ea6a772c811d0b85b6385f8ee4d5d21ad5642
WavuaW <wavuamuroka@gmail.com>
2023-07-11 20:37:30 +0300

    sequential cases

---
+++ 0x01JS/seqSizes.js
@@ -0,0 +1,17 @@
+function sequentialSizes(val) {
+    var answer = "";
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Medium";
+            break;
+    }
+}
+
+console.log(sequentialSizes(6));
