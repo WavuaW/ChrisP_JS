72c92d4771f6cae50416942361a37234ec6aa666
WavuaW <wavuamuroka@gmail.com>
2023-07-12 13:23:24 +0300

    Card counting shell

--- 0x01JS/countingCards.js
+++ 0x01JS/countingCards.js
@@ -1,6 +1,26 @@
 function cc(card) {
-    
-    return "Change Me";
+    switch(card) {
+        case 2:
+        case 3:
+        case 4:  
+        case 5:
+        case 6:
+            count++;
+            break;
+        case 10:
+        case "J":
+        case "K": 
+        case "Q":
+        case "A":  
+            count--;
+            break
+    }
+
+    var holdbet = 'Hold'
+    if (count > 0) {
+        holdbet = 'Bet'
+    }
+    return count + " " + holdbet;
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
