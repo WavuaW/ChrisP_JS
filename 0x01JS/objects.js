1a5a36d5726e73a7035a577d36ed33a0848f75a5
WavuaW <wavuamuroka@gmail.com>
2023-07-12 13:33:19 +0300

    updating properties with dot notation

--- 0x01JS/objects.js
+++ 0x01JS/objects.js
@@ -5,6 +5,8 @@ var ourDog = {
     "friends": ["everything"]
 };
 
+ourDog.name = 'Happy Camper' //dot notation in updating oblect properties
+
 var tail = ourDog.tails;
 var legs = ourDog["legs"]; //Brakect notation is required if the property name has a space in it like 'an entree'
 //Bracket notation can also be used to look objcet properties using variables
