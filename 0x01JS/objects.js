8d9f078ece3ae9598e953f539d6a55b1b793adf8
WavuaW <wavuamuroka@gmail.com>
2023-07-12 13:37:13 +0300

    Deleting an object

--- 0x01JS/objects.js
+++ 0x01JS/objects.js
@@ -5,9 +5,11 @@ var ourDog = {
     "friends": ["everything"]
 };
 
-ourDog.name = 'Happy Camper' //dot notation in updating oblect properties
-ourDog.bark = "bow-wow" //addinf a new property with the dot notation
-ourDog['eats'] = 'Liver'
+ourDog.name = 'Happy Camper'; //dot notation in updating oblect properties
+ourDog.bark = "bow-wow"; //addinf a new property with the dot notation
+ourDog['eats'] = 'Liver';
+
+delete ourDog.bark;
 
 var tail = ourDog.tails;
 var legs = ourDog["legs"]; //Brakect notation is required if the property name has a space in it like 'an entree'
