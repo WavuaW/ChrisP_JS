cfa1091bb5f026393e86c8a501daa84365b4ba98
WavuaW <wavuamuroka@gmail.com>
2023-07-12 13:31:55 +0300

    bracket notation []

--- 0x01JS/objects.js
+++ 0x01JS/objects.js
@@ -7,3 +7,4 @@ var ourDog = {
 
 var tail = ourDog.tails;
 var legs = ourDog["legs"]; //Brakect notation is required if the property name has a space in it like 'an entree'
+//Bracket notation can also be used to look objcet properties using variables
