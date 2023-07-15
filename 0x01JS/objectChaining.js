8efaf0544591f9c99a7b0cafbac0d4f7ea74282d
WavuaW <wavuamuroka@gmail.com>
2023-07-15 12:58:45 +0300

    object chaining

---
+++ 0x01JS/objectChaining.js
@@ -0,0 +1,13 @@
+var myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passanger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+}
+
+var gloveBoxContents = myStorage.car.inside;
