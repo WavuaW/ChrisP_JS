2a726335da178c4d1a2718070628c4456e16be1e
WavuaW <wavuamuroka@gmail.com>
2023-07-17 12:51:51 +0300

    Cenerating random number

--- 0x01JS/genRanFrac.js
+++ 0x01JS/genRanFrac.js
@@ -6,4 +6,14 @@ function randomWholeNum() {
     return Math.random();
 }
 
-console.log(randomWholeNum)
+randomWholeNum
+
+function ourRandomRange(ourMin, ourMax) {
+
+    return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+var myRandom = ourRandomRange(1, 9)
+
+console.log(myRandom)
+
