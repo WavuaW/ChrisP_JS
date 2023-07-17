089bb07aaaf351dd5ae40edf40d7aa0f374be4f6
WavuaW <wavuamuroka@gmail.com>
2023-07-17 13:01:59 +0300

    Randix

--- 0x01JS/convertToInt.js
+++ 0x01JS/convertToInt.js
@@ -1,5 +1,5 @@
 function convertToInteger(str) {
-    return parseInt(str, 2);
+    return parseInt(str, 2); // the second argument is a Randix(specifies the base of the number in the string) that determines which base system the number is in
 }
 
 console.log(convertToInteger("10011"));
