a4c61115d213e8284a20f5cc4a8fc5d15fb504ba
WavuaW <wavuamuroka@gmail.com>
2023-07-17 12:45:00 +0300

    Math, random & floor

---
+++ 0x01JS/genRanFrac.js
@@ -0,0 +1,9 @@
+Math.random() //returns a random number between 0 and one but it cannot be one
+Math.floor(Math.random() * 20); //floor rounds down to the nearest whole number
+
+function randomWholeNum() {
+
+    return Math.random();
+}
+
+console.log(randomWholeNum)
