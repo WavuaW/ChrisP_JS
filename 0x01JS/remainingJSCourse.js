5f9811c628eaf9d7da5e3f8004a0e36c8aa5eca1
WavuaW <wavuamuroka@gmail.com>
2023-07-25 11:51:32 +0300

    rest operator

--- 0x01JS/remainingJSCourse.js
+++ 0x01JS/remainingJSCourse.js
@@ -56,3 +56,12 @@ const squareList = (arr) => {
 
 const squareIntegers = squareList(realNumberArray);
 console.log(squareIntegers)
+
+//rest operator
+const sum = (function () {
+return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+};
+}) ();
+
+console.log(sum(1, 2, 3));
