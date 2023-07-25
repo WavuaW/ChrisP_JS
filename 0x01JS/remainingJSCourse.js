1479b6eca98c364c9c4dbe6aff8d213618f1f0be
WavuaW <wavuamuroka@gmail.com>
2023-07-25 11:18:24 +0300

    Multi Ternary operator

--- 0x01JS/remainingJSCourse.js
+++ 0x01JS/remainingJSCourse.js
@@ -1,2 +1,7 @@
 //Ternary operator
 return a === b ? true : false
+
+//multi conditional ternary operator
+function checjSign(num) {
+    return num > 0 ? 'positive' : num < 0 ? 'negative' : 'zero'
+}
