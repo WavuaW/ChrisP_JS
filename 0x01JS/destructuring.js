928bb5cc5faaeb560161351825af67435e254f30
WavuaW <wavuamuroka@gmail.com>
2023-07-31 10:25:28 +0300

    old and  new way of destructuring

---
+++ 0x01JS/destructuring.js
@@ -0,0 +1,10 @@
+var voxel = {x: 3.6, y: 7.4, z: 6.54};
+
+//old way of destructuring.
+var x = voxel.x;
+var y = voxel.y;
+var z = voxel.z;
+
+//new way of destructuring
+const { x : a, y: b, z : c } = voxel;
+
