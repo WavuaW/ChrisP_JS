4cfdac011286385e07a50433cf46037a33e5bb98
WavuaW <wavuamuroka@gmail.com>
2023-07-31 11:15:12 +0300

    using template literals with for loop

--- 0x01JS/templateLiterals.js
+++ 0x01JS/templateLiterals.js
@@ -6,3 +6,22 @@ const person = {
 const greeting = `Hello, my name is ${person.name}! I am ${person.age} years old.`;
 
 console.log(greeting);
+
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["id-blacklist", "no-duo-keys"]
+};
+
+function makeList(arr) {
+    const resultDisplayArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        resultDisplayArray.push(`<li class='text-warning>${arr[i]}</li>`)
+    }
+
+    return resultDisplayArray;
+}
+
+const resultDisplayArray = makeList(result.failure);
+console.log(resultDisplayArray);
