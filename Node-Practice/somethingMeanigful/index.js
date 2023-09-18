287909a4325f079b5b9a5eab5972bf58b8c384db
WavuaW <wavuamuroka@gmail.com>
2023-09-18 12:19:27 +0300

    few basics

--- Node-Practice/somethingMeanigful/index.js
+++ Node-Practice/somethingMeanigful/index.js
@@ -4,3 +4,15 @@ process.on('exit', function() {
     //example of using process
 })
 
+//creating your own evernt
+
+const { EventEmitter } = require('events');
+
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('lunch', () => {
+    console.log('yum 🤤')
+})
+
+eventEmitter.emit('lunch');
+eventEmitter.emit('lunch');
