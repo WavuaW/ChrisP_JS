d0e902382f07a7b4127e46daafe5b5b2dfb6d775
WavuaW <wavuamuroka@gmail.com>
2023-09-18 12:55:19 +0300

    simple fullstack node and express app

--- Node-Practice/somethingMeanigful/index.js
+++ Node-Practice/somethingMeanigful/index.js
@@ -38,7 +38,7 @@ const app = express()
 app.get('/', (request, response) => {
     readFile('/home.html', 'utf8', (err, html) => {
         if (err) {
-            response.status(500)
+            response.status(500).send('sorry, out of order')
         }
         response.send(html);
     })
