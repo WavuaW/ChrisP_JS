0eae6f4cddc16cb99187bf02c08b26433cd0542a
WavuaW <wavuamuroka@gmail.com>
2023-09-18 12:37:49 +0300

    reading file

--- Node-Practice/somethingMeanigful/index.js
+++ Node-Practice/somethingMeanigful/index.js
@@ -1,19 +1,28 @@
-console.log('hello World')
+const { readFile } = require("fs").promises
 
-process.on('exit', function() {
-    //example of using process
-})
+// console.log('hello World')
+
+// process.on('exit', function() {
+//     // example of using process
+// })
+
 
 //creating your own evernt
 
-const { EventEmitter } = require('events');
+// const { EventEmitter } = require('events');
 
-const eventEmitter = new EventEmitter();
+// const eventEmitter = new EventEmitter();
 
-eventEmitter.on('lunch', () => {
-    console.log('yum 🤤')
-})
+// eventEmitter.on('lunch', () => {
+//     console.log('yum 🤤')
+// })
 
+// eventEmitter.emit('lunch');
+// eventEmitter.emit('lunch');
 
-eventEmitter.emit('lunch');
-eventEmitter.emit('lunch');
+readFile('./hello.txt', 'utf-8', (err, txt) => {
+    console.log(txt)
+});
+
+console.log('do this ASAP')
+
